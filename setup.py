aed323d400d90ff04ed95360231938235c7be6d8
dalefrieswthat <developer@frieswthat.com>
2025-03-28 17:45:48 -0500

    Bump version to 2.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="kubed",
-    version="2.1.3",
+    version="2.1.4",
     packages=find_packages(),
     include_package_data=True,
     entry_points={
